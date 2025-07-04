486fef0d5f0f9b10ebd0537d116519873cf37e13
Abdullah Tayyab <abdullahtayyab414@gmail.com>
2025-07-04 23:55:40 +0500

    done

---
+++ Javascript/JavascriptBasics/array.js
@@ -0,0 +1,83 @@
+// 1 : Array Creation: Create an array of 5 favorite colors and print it to the console.
+
+// let arr = ["White", "Green", "Blue", "Yellow", "Black"];
+//     console.log(arr)
+
+// 2 : Accessing Elements: Declare an array of 6 numbers. Log the first, last, and third elements to the console.
+
+// let arr =[1,2,3,4,5,6];
+// console.log(arr[0],arr[2],arr[arr.length-1])
+
+// 3 : Array Length: Create an array of 10 items. Use the length property to log how many items are in the array.
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(arr.length)
+
+// 4 : Adding Elements: Create an array of 3 fruits. Use push to add two more fruits and log the updated array.
+
+// let arr = ["Banana","Mango"];
+// arr.push("Apple","Guava")
+// console.log(arr);
+
+// 5 : Removing Elements: Create an array of 5 animals. Use pop to remove the last element and log the removed element and the updated array.
+
+// let arr = ["Cat","Dog","Pigon","Lion"];
+// let pop = arr.pop();
+// console.log(arr)
+// console.log("Popped element is " + pop)
+
+//  6 : Array Concatenation: Create two arrays of numbers (e.g., [1, 2, 3] and [4, 5, 6]). Use concat to merge them and log the result.
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
+// 7 : Looping Through Array: Create an array of 5 names. Use a for loop to print each name to the console.
+
+// let arr = ["Abdullah","Tayyab","Fatima","Sheraz","Ibrahim"]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// 8 : Finding an Element: Create an array of 10 numbers. Use includes to check if the number 5 is in the array and log the result.
+
+// let arr= [1,2,3,4,5,6];
+// console.log(arr.includes(7))
+// console.log(arr.includes(4))
+
+// 9 : Array Slicing: Create an array of 8 items. Use slice to extract elements from index 2 to 5 and log the result.
+
+// let arr = ["Abdullah", "Tayyab","Fatima","Sheraz","Ibrahim"]
+// let slice = arr.slice(2,5)
+// console.log(slice)
+
+// 10 : Updating Elements: Create an array of 5 scores. Update the third score to 100 and log the updated array.
+
+// let arr = [23,45,43,56,76,78,98];
+// arr[2]=100;
+// console.log(arr)
+
+// 11 : Array Iteration with forEach: Create an array of 5 prices. Use forEach to print each price with a "$" prefix.
+
+// let arr = [12,21,34,54,67,87,89,90];
+
+// arr.forEach(element => {
+//     console.log("$ " + element)
+// });
+
+// 12 : Mapping an Array: Create an array of 6 numbers. Use map to create a new array with each number squared.
+
+// let arr = [1,2,3,4,5,6];
+// arr.map((item,index)=>{
+// console.log(item * item)
+// })
+
+// 13 : Filtering an Array: Create an array of 10 numbers. Use filter to create a new array containing only even numbers.
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// arr.filter((item)=>{
+//     if(item%2==0){
+//         console.log(item)
+//     }
+// })
