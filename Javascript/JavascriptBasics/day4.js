62a3a7fff11e49315f0cdcd217724507a6e28d2a
Abdullah Tayyab <abdullahtayyab414@gmail.com>
2025-07-16 23:09:15 +0500

    done

---
+++ Javascript/JavascriptBasics/day4.js
@@ -0,0 +1,56 @@
+// 1.String Length: Log the length of a stadium name. 
+
+// let str = "stadium";
+// console.log(str.length)
+
+// 2.Uppercase Name: Convert a player’s name to uppercase.
+
+// let name = "abdullah";
+// console.log(name.toUpperCase())
+
+// 3.Trim Input: Remove whitespace from a user’s input string.
+
+// let str = "Abdullah Tayyab";
+// let str1 = str.replaceAll(" ","")
+// console.log(str1)
+
+// 4.Split Sentence: Split a match description into an array of words. 
+
+// let str = "Abullah Tayyab";
+// console.log(str.split())
+
+// 5.Replace Word: Replace "cricket" with "match" in a string.
+
+// let str = "Cricket Club";
+// console.log(str.replace("Cricket","Match"))
+
+// 6.Check Prefix: Check if a string starts with "Team" using startsWith.
+
+// let str = "Fa**ma";
+// console.log(str.startsWith("F"))
+
+// 7.Extract Substring: Extract the first 4 characters of a team name. 
+
+// let str = "The lion is a king of forest";
+// console.log(str.substring(0,4))
+
+// 8.Join Words: Join an array of words into a sentence with spaces.
+
+// let str = ["Abdullah","Tayyab","Fatima","Sheraz","Ibrahim"]
+// let sen = "";
+// for(let i=0; i<str.length;i++){
+// sen += str[i] + " ";
+// }
+// console.log(sen)
+
+// 9.Char at Index: Log the second character of a player’s name. 
+
+// let str = "Abdullah";
+// console.log(str.charAt(2))
+
+// 10.Concat Strings: Concatenate a player’s first and last name.
+
+// let fName = "Abdullah";
+// let lName = "Tayyab";
+// let fullName = fName.concat(lName);
+// console.log(fullName)
---
+++ Javascript/JavascriptBasics/day5.js
@@ -0,0 +1,91 @@
+// 1.Greet Player: Write a function to return "Hello, [name]!". 
+
+// function name (a){
+// return "Hello " + a;
+// }
+// console.log(name("Abdullah"))
+
+// 2.Square Score: Write a function to square a match score. 
+
+// function match (score){
+//     return score*score;
+// }
+// console.log(match(301))
+
+// 3.Check Even: Write a function to check if overs are even. 
+
+// function over (over){
+//     if(over%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(over(10))
+
+// 4.Full Name: Return a player’s full name from first and last names. 
+
+// function fullName (a,b){
+//     return a+" "+b;
+// }
+// console.log(fullName("Abdullah","Tayyab"))
+
+// 5.Average Runs: Calculate the average of 3 scores in a function.
+
+// function average (a,b,c){
+//     return (a+b+c)/3
+// }
+// console.log(average(12,34,32))
+
+// 6.Format Date: Format a match date (e.g., "2025-07-15" → "15/07/2025").
+
+// function date (a){
+// return a.getDate() + "/"+a.getMonth()+"/"+a.getFullYear();
+// }
+// let newDate = new Date()
+// console.log(date(newDate))
+
+// 7.Max Score: Find the maximum of two scores using a function.
+
+// function maximam(a, b) {
+//     let max;
+//     if (a > b) {
+//         return max = a;
+//     }
+//     else {
+//         return max = b;
+//     }
+// }
+// console.log(maximam(12, 23))
+
+// 8.Team Slogan: Return a team’s slogan based on its name.
+
+// function slogan (name){
+//     switch(name){
+//         case "Pakistan":
+//             return "Pakistan Zindabad";
+//         case "India":
+//             return "India shame on you";
+//         case "Australia":
+//             return "Australia Australia hai";
+//         default:
+//             return "Invalid Country";
+//     }
+
+// }
+// console.log(slogan("Australia"))
+
+// 9.Run Rate: Calculate run rate (runs/overs) in a function.
+
+// function runRate (runs,overs){
+//     return runs/overs;
+// }
+// console.log(runRate(120,9))
+
+// 10.Check Valid: Check if a score is between 0 and 300.
+
+// function scoreValidation(a) {
+//     return a > 0 && a <= 300 ? "True" : "False"
+// }
+// console.log(scoreValidation(122))
