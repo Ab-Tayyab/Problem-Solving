6d8e62ca5cdd3478a2c93432bcb3128bb0f95907
Abdullah Tayyab <abdullahtayyab414@gmail.com>
2025-07-17 23:59:24 +0500

    done

---
+++ Javascript/JavascriptBasics/day6.js
@@ -0,0 +1,27 @@
+// 1.Player Object: Create an object with name, runs, and team.
+
+// let player ={
+//     name:"Abdullah",
+//     runs:300,
+//     team:"Pakistan"
+// }
+// console.log(player)
+
+// 2.Access Runs: Log the runs property of a player object.
+
+// let player ={
+//     name:"Abdullah",
+//     runs:300,
+//     team:"Pakistan"
+// }
+// console.log(player.runs)
+
+// 3.Update Team: Update a player’s team property.
+
+let player ={
+    name:"Abdullah",
+    runs:300,
+    team:"Pakistan"
+}
+player.team="New Zeeland";
+console.log(player)
