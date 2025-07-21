4d0ab0c82d6664bffe32b9137ccb55e42354087b
Abdullah Tayyab <abdullahtayyab414@gmail.com>
2025-07-21 23:58:03 +0500

    done

--- Javascript/JavascriptBasics/day7.js
+++ Javascript/JavascriptBasics/day7.js
@@ -1,6 +1,21 @@
 // 1.Win Check: Check if team score > 200 and log "High Score".
 
-let score = 201;
-if(score>200){
-    console.log("High Score")
+// let score = 201;
+// if(score>200){
+//     console.log("High Score")
+// }
+
+// 2.Match Type: Use switch to log rules for "Test", "ODI", or "T20".
+
+let match = "ODI";
+switch(match){
+    case "Test":
+        console.log("5 Day match, 90 overs per day")
+        break;
+    case "ODI":
+        console.log("One Day, 50 over per team");
+        break;
+    case "T20":
+        console.log("T20 Match, 20 over per team")
+        break;
 }
