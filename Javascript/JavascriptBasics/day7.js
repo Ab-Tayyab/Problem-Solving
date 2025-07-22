03560797c9620298c14d7b50f3007856e3c15010
Abdullah Tayyab <abdullahtayyab414@gmail.com>
2025-07-22 23:58:11 +0500

    done

--- Javascript/JavascriptBasics/day7.js
+++ Javascript/JavascriptBasics/day7.js
@@ -7,15 +7,19 @@
 
 // 2.Match Type: Use switch to log rules for "Test", "ODI", or "T20".
 
-let match = "ODI";
-switch(match){
-    case "Test":
-        console.log("5 Day match, 90 overs per day")
-        break;
-    case "ODI":
-        console.log("One Day, 50 over per team");
-        break;
-    case "T20":
-        console.log("T20 Match, 20 over per team")
-        break;
-}
+// let match = "ODI";
+// switch(match){
+//     case "Test":
+//         console.log("5 Day match, 90 overs per day")
+//         break;
+//     case "ODI":
+//         console.log("One Day, 50 over per team");
+//         break;
+//     case "T20":
+//         console.log("T20 Match, 20 over per team")
+//         break;
+// }
+
+// 3.Count Players: Count players with runs > 50 in an array using a loop.
+
+let scores=[123,32,23,51,65,45,76,49];
